104f2972e5046ca5355b3fe1a224e78e2cc36a25
bonddenhalter <bonddenhalter@gmail.com>
2020-09-11 19:26:40 -0600

    finished part 1 and 2 (Bond's version)

---
+++ CS6360_Assignment1-1/Assets/BallDropScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDropScript : MonoBehaviour
+{
+    public GameObject ball;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rigidbody = ball.GetComponent<Rigidbody>();
+        rigidbody.useGravity = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CS6360_Assignment1-1/Assets/Lightswitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour
+{
+
+    public Light light;
+    uint curColorIndex = 0;
+    Color[] colors = { Color.cyan, Color.yellow, Color.green, Color.red, Color.blue, Color.white, Color.magenta };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("tab"))
+        {
+            if(++curColorIndex >= colors.Length)
+            {
+                curColorIndex = 0;
+            }
+            light.color = colors[curColorIndex];
+        }
+    }
+}
